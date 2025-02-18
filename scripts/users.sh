1fcd07d853c7e7381df10e0ca32f9a13893595ba
A-Coder <htetarkarkyaw@yahoo.com>
2025-02-17 23:27:46 -0500

    Hello acoder

--- scripts/users.sh
+++ scripts/users.sh
@@ -1,12 +1,25 @@
-username="ans2"
+username="ant3"
 password="Ans@123"
 
-touch /etc/sudoer.d/$username
+touch /etc/sudoers.d/$username
 # Check if user already exists
 if id $username &>/dev/null; then
     echo "User $username already exists!" >&2
     exit 1
 fi
+
+#!/bin/bash
+
+file= /etc/sysconfig/iptables.BK
+if [ -e $file ]; then
+    echo "File exists. Setting permissions..."
+else
+    echo "File does not exist. Creating file..."
+    touch $file
+fi
+
+temp = /etc/sysconfig/temp
+
 
 # Create the user
 useradd -m -s /bin/bash $username
@@ -15,10 +28,25 @@ echo "User $username created successfully."
 echo $username:$password | chpasswd
 
 echo "Password set successfully."
-
+chown $username /etc/sysconfig/ 
+chown $username $temp
 chown $username /etc/sysconfig/iptables
+chown $username /etc/sysconfig/iptables.BK
 # Add the user to the group
 sudo usermod -aG root $USERNAME
+
+echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl  start  iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl reload iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl reload iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl restart iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl  start  iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl status iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl  status  iptables" | sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD:  /bin/systemctl is-active iptables"| sudo tee -a /etc/sudoers.d/$username > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD:  /usr/bin/systemctl is-active iptables"| sudo tee -a /etc/sudoers.d/$username > /dev/null
+
+
 
 echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl  start  iptables" | sudo tee -a /etc/sudoers > /dev/null
 echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl reload iptables" | sudo tee -a /etc/sudoers > /dev/null
@@ -26,3 +54,8 @@ echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart iptables" | sudo
 echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl reload iptables" | sudo tee -a /etc/sudoers > /dev/null
 echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl restart iptables" | sudo tee -a /etc/sudoers > /dev/null
 echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl  start  iptables" | sudo tee -a /etc/sudoers > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /bin/systemctl status iptables" | sudo tee -a /etc/sudoers > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD: /usr/bin/systemctl  status  iptables" | sudo tee -a /etc/sudoers > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD:  /bin/systemctl is-active iptables"| sudo tee -a /etc/sudoers > /dev/null
+echo "$username  ALL=(ALL) NOPASSWD:  /usr/bin/systemctl is-active iptables"| sudo tee -a /etc/sudoers > /dev/null
+
